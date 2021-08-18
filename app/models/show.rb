9638a217dddc1c85e8b7198e88dd12d578b2abf1
Seungil <schoi0115@gmail.com>
2021-08-17 20:33:56 -0400

    completed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map do |t|
+            "#{t.first_name} #{t.last_name}"
+        end
+    end
 end
